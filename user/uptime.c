4e827289a9114cc79ff0d3d5cf8f90b503c7bf35
Bryan Mendoza <bmendoza7@miners.utep.edu>
2023-09-14 13:57:17 -0600

    Finished Hw1, uptime.c, Makefile were modified

--- user/uptime.c
+++ user/uptime.c
@@ -1,8 +1,9 @@
+
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[]){
+int main(char argc,  char *argv[]){
 int count=0;
 count=atoi(argv[1]);
 count=uptime();
